9136c2154ae96e4a290788ff288815e9ce93c092
Raviraj Wadnerkar <ravirajwadnerkar@gmail.com>
2021-07-04 15:06:33 +0530

    learning js

---
+++ index.js
@@ -0,0 +1,20 @@
+document.query
+//alert("Hello");
+document.querySelector("h1").innerHTML = "Good Bye";
+//document.querySelector("input").click();
+//document.firstElementChild.lastElementChild.firstElementChild;
+//document.getElementsByTagName("li")[2].style.color = "purple";
+//document.getElementsByClassName("item")[2].style.color = "blue";
+//document.getElementById("list").innerHTML = "Behind YOU!!!!!!!!"
+//document.getElementById("title").innerHTML = "Behind YOU!!!!!!!!";
+//document.querySelector(".item a").style.color = "red";
+//document.querySelector("h1").style.fontSize = "10px";
+//document.querySelector("h1").style.fontStyle = "calibri";
+//document.querySelector("h1").style.visibility = "hidden";
+//document.querySelector("button").style.backgroundColor = "yellow";
+//document.querySelector("button").classList.add("invisible");
+//document.querySelector("button").classList.remove("invisible");
+//document.querySelector("button").classList.toggle("invisible");
+//document.querySelector("h1").textContent;
+//document.querySelector("a").getAttribute("href");
+//document.querySelector("a").setAttribute("href,https://www.github.com/ravirajwadnerkar");
